7ccf8b3a1fdca7f86a4a64f83ac593dd5c96615f
johnnycuongn <cuongdn2001@gmail.com>
2020-04-15 21:39:59 +1000

    Initial Commit

---
+++ Multiple Choice Questions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Choice Questions
+//
+//  Created by Johnny on 15/4/20.
+//  Copyright © 2020 Johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
